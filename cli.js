f9c6cfc9f1002718832aaaa9be7060bbceb6fe6f
David Rubin <david@unumotors.com>
2020-06-27 13:54:11 +0200

    Add  basic  CLI framework and parsing

--- cli.js
+++ cli.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log('Hail Hydra')
+const yargs = require('yargs')
+const replication = require('./lib/cli/replication')
+
+// This seems required to parse
+// eslint-disable-next-line no-unused-vars
+const { argv } = yargs
+  .command(replication)
+  .demandCommand()
+  .help()
+  // Disable wrapping
+  .wrap(null)
+  .epilog('Hail Hydra\ncopyright 2020 unu GmbH')
---
+++ test/cli/replication.test.js
@@ -0,0 +1,27 @@
+const test = require('ava')
+const yargs = require('yargs')
+const replicationCmd = require('../../lib/cli/replication')
+
+test('returns help output', (t) => {
+  t.plan(1)
+  // Initialize parser using the command module
+  const parser = yargs.command(replicationCmd).help()
+
+  // Run the command module with --help as argument
+  parser.parse('--help', (err, argv, out) => {
+    // Just test that the help text contains replication
+    t.regex(out, /Sets up replication/)
+  })
+})
+
+test('parses replica members', (t) => {
+  t.plan(1)
+  // Initialize parser using the command module
+  const parser = yargs.command(replicationCmd).help()
+
+  // Run the command module with --help as argument
+  parser.parse('-r=mongo-0:27017 -r=mongo-1:27017 -r=mongo-1:27017', (err, argv) => {
+    // Just test that the help text contains replication
+    t.deepEqual(argv.r, ['mongo-0:27017', 'mongo-1:27017', 'mongo-1:27017'])
+  })
+})
