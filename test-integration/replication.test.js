f6069d9da6b3deac1033d85209210831e2d5fdb2
Immanuel Pelzer <immanuel@unumotors.com>
2020-06-28 13:09:06 +0200

    Refactor to class based structure

--- test-integration/replication.test.js
+++ test-integration/replication.test.js
@@ -11,57 +11,38 @@
  */
 const test = require('ava')
 
-const { MongoServerState } = require('../lib/core/state/mongo-server')
-const MongoClient = require('../lib/core/clients/mongodb-client')
-
 const { ReplicaSetState, REPLICA_SET_STATUS } = require('../lib/core/state/replication')
-const { ReplicationConfiguration } = require('../lib/core/structures/replication-structures')
 
 const replicaSetName = 'replication-test'
-const data = [
+const servers = [
   {
-    hostname: 'replication-test-0:27000',
+    host: 'replication-test-0:27000',
     uri: 'mongodb://localhost:27000'
   },
   {
-    hostname: 'replication-test-1:27001',
+    host: 'replication-test-1:27001',
     uri: 'mongodb://localhost:27001'
   },
   {
-    hostname: 'replication-test-2:27002',
+    host: 'replication-test-2:27002',
     uri: 'mongodb://localhost:27002'
   }
 ]
-const servers = []
-
-for (let index = 0; index < data.length; index += 1) {
-  const { hostname, uri } = data[index]
-  const client = new MongoClient(uri)
-  const server = new MongoServerState(hostname, client)
-  data[index].server = server
-  servers.push(server)
-}
 
 test.serial('ReplicaSetState can pick up uninitialized state', async (t) => {
-  const configuration = new ReplicationConfiguration({ replicaSetName, servers })
-
-  const state = new ReplicaSetState(configuration)
+  const state = new ReplicaSetState({ replicaSetName, servers })
   await state.refreshState()
   t.is(state.replicaSetStatus, REPLICA_SET_STATUS.UNINITIALIZED)
 })
 
 test.serial('ReplicaSetState can initialize replica set', async (t) => {
-  const configuration = new ReplicationConfiguration({ replicaSetName, servers })
-
-  const state = new ReplicaSetState(configuration)
+  const state = new ReplicaSetState({ replicaSetName, servers })
   await state.apply()
   t.is(state.replicaSetStatus, REPLICA_SET_STATUS.HEALTHY)
 })
 
 test.serial('ReplicaSetState can detect a healthy replica set', async (t) => {
-  const configuration = new ReplicationConfiguration({ replicaSetName, servers })
-
-  const state = new ReplicaSetState(configuration)
+  const state = new ReplicaSetState({ replicaSetName, servers })
   await state.refreshState()
   t.is(state.replicaSetStatus, REPLICA_SET_STATUS.HEALTHY)
 })
--- test/core/state/mongo-server.test.js
+++ test/core/state/mongo-server.test.js
@@ -1,108 +1,19 @@
 const test = require('ava')
-const MongoClientStub = require('../../../lib/core/clients/stub-client')
-const { MongoServerState, SERVER_REPLICATION_STATUS } = require('../../../lib/core/state/mongo-server')
+const MongoClient = require('../../../lib/core/clients/mongodb-client')
+const MongoServerState = require('../../../lib/core/state/mongo-server')
 
-const hostname = 'mongo-server:1'
+const host = 'mongo-server:1'
 
-test('mongo server class initializes with UNKNOWN state', async (t) => {
-  const client = new MongoClientStub([])
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.UNKNOWN)
+test('mongo server creates MongoClient instance by default', (t) => {
+  const server = new MongoServerState({ host })
+  t.true(server.client instanceof MongoClient)
+  t.is(server.client.uri, host)
 })
 
-test('mongo server class detect that it cannot handle an error', async (t) => {
-  const unknownError = new Error('unhandled-error')
-
-  const replies = [
-    unknownError
-  ]
-  const client = new MongoClientStub(replies)
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-
-  await t.throwsAsync(async () => {
-    await server.refreshState()
-  }, { message: unknownError.message })
-})
-
-test('mongo server class can detect UNINITIALIZED state', async (t) => {
-  const uninitializedError = new Error()
-  uninitializedError.code = 94
-
-  const replies = [
-    uninitializedError
-  ]
-  const client = new MongoClientStub(replies)
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-
-  await server.refreshState()
-
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.UNINITIALIZED)
-})
-
-test('mongo server class refreshState throws if it encounters an unknown mongo error status code', async (t) => {
-  const unknownError = new Error('unknown')
-  unknownError.code = 666
-
-  const replies = [
-    unknownError
-  ]
-  const client = new MongoClientStub(replies)
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-
-  await t.throwsAsync(async () => {
-    await server.refreshState()
-  }, unknownError)
-})
-
-test('mongo server class can detect valid mongo states', async (t) => {
-  const replies = [
-    { myState: 0 },
-    { myState: 1 },
-    { myState: 2 },
-    { myState: 3 },
-    { myState: 5 },
-    { myState: 6 },
-    { myState: 7 },
-    { myState: 8 },
-    { myState: 9 }
-  ]
-  const client = new MongoClientStub(replies)
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.STARTUP)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.PRIMARY)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.SECONDARY)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.RECOVERING)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.STARTUP2)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.UNKNOWN)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.ARBITER)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.DOWN)
-  await server.refreshState()
-  t.is(server.replicationStatus, SERVER_REPLICATION_STATUS.ROLLBACK)
-})
-
-test('mongo server class throws if it cannot handle mongo reply', async (t) => {
-  const replies = [
-    { unknown: 'response' }
-  ]
-  const client = new MongoClientStub(replies)
-  await client.connect() // does nothing
-  const server = new MongoServerState(hostname, client)
-
-  await t.throwsAsync(async () => {
-    await server.refreshState()
-  }, { message: 'Unknown state of replica member: {"unknown":"response"}' })
+test('mongo server can overwrite client uri', (t) => {
+  const uri = 'uri-123'
+  const server = new MongoServerState({ host, uri })
+  t.is(server.uri, uri)
+  t.is(server.host, host)
+  t.is(server.client.uri, uri)
 })
